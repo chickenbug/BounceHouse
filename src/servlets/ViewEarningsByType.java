e82ec4fe9815f73dd2563f9bc5d4969de51f82a8
Tim Goetjen <tgoetjen@gmail.com>
2016-05-01 00:38:15 -0400

    about to pull kinh

--- src/servlets/ViewEarningsByType.java
+++ src/servlets/ViewEarningsByType.java
@@ -19,72 +19,56 @@ import javax.servlet.http.HttpServletResponse;
 import model.SQLConnector;
 
 /**
- * Servlet implementation class ViewAuctions
+ * Servlet implementation class ViewEarningsByType
  */
-@WebServlet(name = "ViewTotalEarnings", urlPatterns = {"/ViewTotalEarnings"})
-public class ViewTotalEarnings extends HttpServlet {
+@WebServlet("/ViewEarningsByType")
+public class ViewEarningsByType extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getSession().getAttribute("userID") == null||!request.getSession().getAttribute("role").equals("admin")) {
+		if (request.getSession().getAttribute("userID") == null) {
 			response.sendError(403, "You are not authorized to access this page.");
 		}
-
-		//PrintWriter to write to HTML.
 		PrintWriter writer  = response.getWriter();
-
-		/*
-		 * Set up page title and links. The structure of the println statement is only to mimic 
-		 * the structure of a formatted HTML document. This structure is only maintained below 
-		 * when it is conducive to readability; when this structure inhibits code readability,
-		 * I do not use it.
-		 */
+		
 		writer.println("<html>" 
 				+	"<head>" 
 				+		"<title>" 
-				+			"Bouncehouse Emporium - Total Earnings" 
+				+			"Bouncehouse Emporium - Total Earnings By Type" 
 				+		"</title>" 
 				+	"</head>" 
 				+	"<body>" 
 				+		"<center>" 
 				+			"<h1>Bouncehouse Emporium</h1>" 
-				+			"<h3>Total Earnings</h3>"
+				+			"<h3>Total Earnings By Type</h3>"
 				+			"<hr>"
 				+			"<table border = 1 width = 100%>"
 				);
-
-		//Create objects for connections, statements, and resultsets.
+		
 		Connection connection = null;
-		Statement getAuctions = null;
-		ResultSet auctions = null;
+		Statement getEarnings = null;
+		ResultSet earnings = null;
 
 		//Open connection, create statement, and process request.
 		try {
 			connection = SQLConnector.getConnection();
-			getAuctions = connection.createStatement();
-
-			auctions = getAuctions.executeQuery("SELECT A.WinBid,U.Username FROM Auction A,User U WHERE Completed = 1 AND A.UserID=U.UserID;");
-
-
+			getEarnings = connection.createStatement();
+			String query = "";
+			
+			if (request.getParameter("sortBy") == null || request.getParameter("sortBy").equals("noSort")) {
+				query.concat("ORDER BY ItemID ASC"); //sort in terms of itemID number low to high if no sorting specified.
+			} else if (request.getParameter("sortBy").equals("bouncinessASC")) { //Bounciness low to high
+				query.concat("ORDER BY Bounciness ASC;");
+			}
 			/*
 			 * Process request and output HTML.
 			 */
 			writer.println("<tr>"
 					+		"<td><center><span style = \"font-weight:bold\"> Username </span></center></td>"
 					+		"<td><center><span style = \"font-weight:bold\"> Earnings </span></center></td>");
-
-
-			Map<String,Integer> users = new HashMap<String,Integer>();
-			while(auctions.next()){
-				if(users.containsKey(auctions.getString("U.username"))){
-					users.put(auctions.getString("U.username"),users.get(auctions.getString("U.username"))+auctions.getInt("A.WinBid"));
-				}
-				else users.put(auctions.getString("U.username"), auctions.getInt("A.WinBid"));
-			}
-			for(Entry<String, Integer> entry: users.entrySet()){
-				writer.println("<tr><td><center>" + entry.getKey() + "</center></td>"
-						+		"<td><center>" + entry.getValue() + "</center></td>");
-			}
 
 			writer.println("</table>"
 					+ "<br>"
@@ -99,11 +83,11 @@ public class ViewTotalEarnings extends HttpServlet {
 		} finally {
 			//Close resultset, statement, connection.
 			try {
-				if (auctions != null) {
-					auctions.close();
+				if (earnings != null) {
+					earnings.close();
 				}
-				if (getAuctions != null) {
-					getAuctions.close();
+				if (getEarnings != null) {
+					getEarnings.close();
 				}
 				if (connection != null) {
 					connection.close();
@@ -129,7 +113,11 @@ public class ViewTotalEarnings extends HttpServlet {
 		}
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
